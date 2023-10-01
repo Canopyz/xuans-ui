4b73ae33ca0910e24550259ad59e2f3f34078c55
Canopyz <2575744741@qq.com>
2023-10-01 19:20:52 +0800

    feat: button

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import type { ButtonInstance } from './components/Button/types'
+import Button from './components/Button/button.vue'
+
+const button = ref<ButtonInstance>()
+onMounted(() => {
+  if (button.value) {
+    console.log('buttonRef', button.value.ref)
+  }
+})
+</script>
+
+<template>
+  <Button type="primary" round ref="button"> 123 </Button>
+  <Button type="danger"> 123 </Button>
+  <Button type="info"> 123 </Button>
+  <Button type="success"> 123 </Button>
+  <Button type="warning"> 123 </Button>
+  <br />
+  <a href="">link</a>
+  <h1>Test</h1>
+  <h2>Test</h2>
+  <h3>Test</h3>
+  <hr />
+</template>
+
+<style scoped></style>
---
+++ src/components/Button/button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    ref="button"
+    class="xs-button"
+    :class="{
+      [`xs-button--${type}`]: true,
+      [`xs-button--${size}`]: true,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { defineOptions, ref } from 'vue'
+import type { ButtonProps } from './types'
+
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button',
+})
+
+defineOptions({
+  name: 'XsButton',
+})
+
+const button = ref<HTMLButtonElement>()
+
+defineExpose({
+  ref: button,
+})
+</script>
+
+<style scoped></style>
